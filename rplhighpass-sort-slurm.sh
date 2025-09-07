98067288c54226b7f31cdc9ae4cf57e3b852ea65
AWS ParallelCluster user <ec2-user@ip-10-0-6-41.ap-southeast-1.compute.internal>
2025-09-07 12:55:52 +0000

    Modify slurm script to use conda envs

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -13,6 +13,13 @@
 #SBATCH -e rplhpsort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -23,4 +30,6 @@ export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
 
 
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
 
